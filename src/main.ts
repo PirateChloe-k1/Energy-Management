ed66f16b52517a467a108a3e00ada4efe0ff02bb
PirateChloe <piratechloe04@outlook.com>
2025-08-15 01:15:12 +0800

    登录功能后端接口处理

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@ import router from './router/index.js'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 
+const pinia = createPinia()
+
 app.use(ElementPlus)
+app.use(pinia)
 app.use(router)
 app.mount('#app')
