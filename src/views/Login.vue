8cb7504c1f68d7d491f918d87ac97e8d618974f4
PirateChloe <piratechloe04@outlook.com>
2025-08-14 19:27:38 +0800

    登录表单数据逻辑处理

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,15 +5,18 @@
                 <img :src="logo" alt="" width="70px" height="70px">
                 <h1 class="ml">动力港能源管理平台</h1>
             </div>
-            <el-form>
-                <el-form-item>
-                    <el-input placeholder="请输入用户名" prefix-icon="User" />
+            <el-form :model="ruleform" :rules="rules" ref="formRef">
+                <el-form-item prop="username">
+                    <!-- 这里的prefix-icon图标可以直接使用，因为已经全局注册了 -->
+                    <!-- 如果没有全局注册而是在单独的文件中引入的话，需要用v-bind绑定,例如 :prefix-icon="User" -->
+                    <el-input v-model="ruleform.username" placeholder="请输入用户名" prefix-icon="User" />
+                </el-form-item>
+                <el-form-item prop="password">
+                    <!-- 使用type="password"实现密码的隐藏 -->
+                    <el-input v-model="ruleform.password" placeholder="请输入密码" prefix-icon="Lock" type="password"/>
                 </el-form-item>
                 <el-form-item>
-                    <el-input placeholder="请输入密码" prefix-icon="Lock"/>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" style="width: 100%;">登录</el-button>
+                    <el-button type="primary" style="width: 100%;" @click="handleLogin">登录</el-button>
                 </el-form-item>
             </el-form>
         </div>
@@ -21,7 +24,39 @@
 </template>
 <script setup lang="ts">
 import logo from "@/assets/logo.png"
+import { reactive, ref } from "vue";
+import { type FormRules,type FormInstance } from "element-plus";
 
+interface RuleForm {
+    username:string,
+    password:string
+}
+
+const ruleform:RuleForm = reactive({
+    username:"",
+    password:""
+})
+
+const rules = reactive<FormRules<RuleForm>>({
+    username:[
+        {required:true,message:"用户名不能为空",trigger:"blur"},
+        {min:3,max:8,message:"用户名要求4-8位数字字母组合",trigger:"blur"}
+    ],
+    password:[
+        {required:true,message:"请输入密码",trigger:"blur"}
+    ]
+})
+
+const formRef=ref<FormInstance>()
+
+const handleLogin = () => {
+    // ?.表示可选链操作符
+    formRef.value?.validate((valid:boolean) => {
+        if(valid){
+            // 校验通过
+        }
+    })
+}
 </script>
 
 <style lang="less" scoped>
