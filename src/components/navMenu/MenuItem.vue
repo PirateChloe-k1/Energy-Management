ec62bd4e3e83bdd578df6661543a48aa61b393fb
PirateChloe <piratechloe04@outlook.com>
2025-08-15 18:10:19 +0800

    侧边导航细节调整

--- src/components/navMenu/MenuItem.vue
+++ src/components/navMenu/MenuItem.vue
@@ -2,14 +2,16 @@
     <el-sub-menu v-if="item.children" :index="item.url">
         <template #title>
             <el-icon>
-                <location/>
+                <component :is="item.icon"></component>
             </el-icon>
             <span>{{ item.name }}</span>
         </template>
         <my-menu v-for="child in item.children" :key="child.url" :item="child"></my-menu>
     </el-sub-menu>
     <el-menu-item v-else :index="item.url">
-        <el-icon><location/></el-icon>
+        <el-icon>
+            <component :is="item.icon"></component>
+        </el-icon>
         <span>{{ item.name }}</span>
     </el-menu-item>
 </template>
@@ -21,6 +23,7 @@ export default defineComponent({
     name:"MyMenu",
     props:{
         item:{
+            // 使用 TypeScript 的 PropType 来指定 item 的具体类型为 MenuItemType
             type:Object as PropType<MenuItemType>,
             required:true    
         }
@@ -29,5 +32,23 @@ export default defineComponent({
 </script>
 
 <style scoped lang="less">
-
+.is-active{
+    background-color: rgb(34, 136, 255);
+    color: #fff !important;
+    div{
+        span{
+            color: #fff;
+        }
+    }
+}
+.el-menu-item:hover{
+    background-color:rgb(34, 136, 255) !important;
+    // 文字颜色
+    color: #fff !important ;
+}
+::v-deep .el-sub-menu__title:hover{
+    // 文字颜色
+    background-color:rgb(34, 136, 255) !important;
+    color: #fff !important ;
+}
 </style>
