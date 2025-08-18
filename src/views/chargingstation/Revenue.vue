acdbd3f05e6496c1edf1d963b06f03ea52a38941
PirateChloe <piratechloe04@outlook.com>
2025-08-18 19:23:34 +0800

    营收统计数据总览开发

--- src/views/chargingstation/Revenue.vue
+++ src/views/chargingstation/Revenue.vue
@@ -1,3 +1,153 @@
 <template>
-    应收统计
+    <div>
+        <el-row :gutter="20">
+            <el-col :span="4">
+                <!-- 今日总收入 -->
+                <el-card>
+                    <div class="title">
+                        <div class="round">
+                            <el-icon>
+                                <Document />
+                            </el-icon>
+                        </div>
+                        <h4>今日总收入（元）</h4>
+                    </div>
+                    <div class="total mt">
+                        <h1>239,824</h1>
+                        <div class="percent" >-21%</div>
+                    </div>
+                </el-card>
+            </el-col>
+            <!-- 本月总收入 -->
+            <el-col :span="4">
+                <el-card>
+                    <div class="title">
+                        <div class="round">
+                            <el-icon>
+                                <Document />
+                            </el-icon>
+                        </div>
+                        <h4>本月总收入 (万元)</h4>
+                    </div>
+                    <div class="total mt">
+                        <h1>2,924</h1>
+                        <div class="percent">-21%</div>
+                    </div>
+                </el-card>
+            </el-col>
+            <el-col :span="4">
+                <!-- 会员卡储值金额 -->
+                <el-card>
+                    <div class="title">
+                        <div class="round">
+                            <el-icon>
+                                <Document />
+                            </el-icon>
+                        </div>
+                        <h4>会员卡储值金额 (元)</h4>
+                    </div>
+                    <div class="total mt">
+                        <h1>239,824</h1>
+                        <div class="percent">-16%</div>
+                    </div>
+                </el-card>
+            </el-col>
+            <el-col :span="4">
+                <!-- 服务费总金额 -->
+                <el-card>
+                    <div class="title">
+                        <div class="round">
+                            <el-icon>
+                                <Document />
+                            </el-icon>
+                        </div>
+                        <h4>服务费总金额 (元)</h4>
+                    </div>
+                    <div class="total mt">
+                        <h1>239,824</h1>
+                        <div class="percent">-7%</div>
+                    </div>
+                </el-card>
+            </el-col>
+            <el-col :span="4">
+                <!-- 停车费总金额 -->
+                <el-card>
+                    <div class="title">
+                        <div class="round">
+                            <el-icon>
+                                <Document />
+                            </el-icon>
+                        </div>
+                        <h4>停车费总金额 (元)</h4>
+                    </div>
+                    <div class="total mt">
+                        <h1>239,824</h1>
+                        <div class="percent">-4%</div>
+                    </div>
+                </el-card>
+            </el-col>
+            <el-col :span="4">
+                <!-- 电费总金额 -->
+                <el-card>
+                    <div class="title">
+                        <div class="round">
+                            <el-icon>
+                                <Document />
+                            </el-icon>
+                        </div>
+                        <h4>电费总金额 (元)</h4>
+                    </div>
+                    <div class="total mt">
+                        <h1>239,824</h1>
+                        <div class="percent">-19%</div>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
 </template>
+<script setup lang="ts">
+
+</script>
+
+<style lang="less">
+.title {
+    display: flex;
+    align-items: center;
+
+    .round {
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        background-color: rgb(235, 236, 245);
+        text-align: center;
+        line-height: 30px;
+        margin-right: 20px;
+    }
+
+    h4 {
+        color: #666;
+    }
+}
+
+.total {
+    display: flex;
+    align-items: center;
+
+    h1 {
+        font-size: 30px;
+        margin-right: 20px;
+    }
+
+    .percent {
+        display: inline-block;
+        padding: 3px 5px;
+        height: 20px;
+        font-size: 12px;
+        background-color: rgb(235, 247, 239);
+        border-radius: 2px;
+        color: green;
+        line-height: 20px;
+    }
+}
+</style>
