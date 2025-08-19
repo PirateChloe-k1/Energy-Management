3ba604445e68d084b8857e8f2f8d7de2a24429ef
PirateChloe <piratechloe04@outlook.com>
2025-08-19 19:36:50 +0800

    地图标记添加信息窗体功能实现

--- src/components/map/MapContainer.vue
+++ src/components/map/MapContainer.vue
@@ -7,9 +7,10 @@
 <script setup lang="ts">
 
 import AMapLoader from '@amap/amap-jsapi-loader';
-import { onMounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 import { mapListApi } from '@/api/map';
 import icon from "@/assets/flashIcon.png"
+import station from "@/assets/station.jpg"
 let map: any = null;
 const markersData = ref<any>([])
 onMounted(() => {
@@ -34,6 +35,11 @@ onMounted(() => {
         mapListApi().then(({ data }) => {
             // 存储所有充电站的数据
             markersData.value = data;
+            // 创建信息窗体
+            const infoWindow = new AMap.InfoWindow({
+                offset: new AMap.Pixel(0, 30)
+            })
+
             // 循环创建标记
             markersData.value.forEach((markerData: any) => {
                 const marker = new AMap.Marker({
@@ -41,12 +47,32 @@ onMounted(() => {
                     icon: icon, //添加 icon 图标 URL
                     title: "北京",
                 });
+                marker.on("click", () => {
+                    // JS代码中使用 ${} 进行模板字符串插值
+                    infoWindow.setContent(`
+                        <div style="display:flex;padding:10px;align-items:center">
+                            <div>
+                                <img src="${station}" width="200px"/>    
+                            </div>
+                            <div style="width:180px;line-height:30px;margin-left:20px">
+                                <h3>${markerData.title}</h3>
+                                <p>充电桩数量：${markerData.count}</p>   
+                                <p>充电站状态：<span style="color:blue">${markerData.status == 1 ? "使用中" : "维护中"}</span></p>    
+                            </div>
+                        </div>
+                    `)
+                    infoWindow.open(map,marker.getPosition())
+                })
                 map.add(marker)
             })
         })
     }).catch((e) => {
         console.log(e);
     });
+})
+
+onUnmounted(()=>{
+    map?.destory()
 })
 </script>
 
