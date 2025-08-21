26bf29e731cd5f78a0ce211f17802d887bc1053a
PirateChloe <piratechloe04@outlook.com>
2025-08-21 09:02:58 +0800

    根据后端接口动态渲染tree树形控件

--- src/views/operations/Total.vue
+++ src/views/operations/Total.vue
@@ -7,19 +7,34 @@
                         <el-button icon="Search" />
                     </template>
                 </el-input>
-                <el-tree ref="treeRef" style="max-width: 600px;" :props="defaultProps"></el-tree>
+                <el-tree 
+                :data="treeData"
+                style="max-width: 600px;" 
+                :props="defaultProps"
+                class="mt"
+                ></el-tree>
             </el-card>
         </el-col>
     </el-row>
 </template>
 
-<script setup>
-import { ref } from 'vue';
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { cityListApi } from '@/api/operation';
+interface Tree{
+    label:string,
+    chileren?:Tree[]
+}
 
 const filterText = ref < string > ("")
-
 const defaultProps = {
-    chileren:'children',
-    label:'label'
+    chileren: 'children',
+    label: 'label'
 }
+
+const treeData = ref<Tree[]>([])
+onMounted(async()=>{
+    const {data} = await cityListApi()
+    treeData.value = data
+})
 </script>
