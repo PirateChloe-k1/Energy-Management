0b661e5c0094598b6c4b5239b2b79086a4fa827d
PirateChloe <piratechloe04@outlook.com>
2025-08-21 08:47:06 +0800

    计费管理页面基本结构开发

--- src/views/operations/Total.vue
+++ src/views/operations/Total.vue
@@ -1,3 +1,25 @@
 <template>
-    计费管理
+    <el-row :gutter="20">
+        <el-col :span="6">
+            <el-card>
+                <el-input style="width: 80%;" placeholder="请输入关键词" v-model="filterText">
+                    <template #append>
+                        <el-button icon="Search" />
+                    </template>
+                </el-input>
+                <el-tree ref="treeRef" style="max-width: 600px;" :props="defaultProps"></el-tree>
+            </el-card>
+        </el-col>
+    </el-row>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const filterText = ref < string > ("")
+
+const defaultProps = {
+    chileren:'children',
+    label:'label'
+}
+</script>
